23d47050681a47551fe93290fc8f62618044279a
Leonard <leonard.horne@gmail.com>
2017-10-02 11:01:51 -0400

    Initial commit. Create events controller.

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'events/show'
+
+  get 'events/new'
+
+  get 'events/create'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
