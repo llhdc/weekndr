63db69ace858ddf7c37fcca32397f0eca14bda20
Leonard <leonard.horne@gmail.com>
2017-10-02 17:34:44 -0400

    Build out foursquare module. Add explore options.

--- app/lib/assets/funsquare.rb
+++ app/lib/assets/funsquare.rb
@@ -6,7 +6,7 @@ require "byebug"
 
 module Foursquare
   class Venue
-    attr_accessor :fid, :name, :location
+    attr_accessor :fid, :name, :location, :rating, :url, :hours, :photourl, :price
   end
 
   class Client
@@ -18,29 +18,38 @@ module Foursquare
         venue.fid = item["venue"]["id"]
         venue.name = item["venue"]["name"]
         venue.location = item["venue"]["location"]
+        venue.rating = item["venue"]["rating"]
+        venue.url = item["venue"]["url"]
+        venue.hours = item["venue"]["url"]
+        venue.photourl = item["tips"][0]["photourl"]
+        venue.price = item["venue"]["price"]
         venue
       end
     end
 
-    def top50(city)
+    def top50(city, section)
       explore(
         city: city,
-        limit: 50
+        limit: 50,
+        section: section,
       )
     end
 
-    def explore(city:, limit: 20)
-      response = get_explore(city: city, limit: limit)
+    def explore(city:, limit: 20, section: nil)
+      response = get_explore(city: city, limit: limit, section: section)
       response_to_venues(response)
     end
 
-    def get_explore(city:, limit:)
+    def get_explore(city:, limit:, section: nil)
+      query = {}
+      query[:near] = city
+      query[:limit] = limit
+      query[:section] = section if section
+
+
       authenticated_get(
         path: "explore",
-        query: {
-          near: city,
-          limit: limit
-        }
+        query: query
       )
     end
 
@@ -63,16 +72,26 @@ module Foursquare
 end
 
 fs = Foursquare::Client.new
-nyc_venues = fs.top50("nyc")
-# nyc_venues.each do |venue|
-#   puts venue.fid
-#   puts venue.name
-#   puts venue.location
-#   puts
-# end
+# outdoors = fs.top50("nyc", "outdoors")
+night = fs.top50("nyc", "nightlife")
+# eat = fs.top50("nyc", "food")
+# arts = fs.top50("nyc", "arts")
+# nyc_venues = fs.top50("nyc")
+# # nyc_venues.each do |venue|
+# #   puts venue.fid
+# #   puts venue.name
+# #   puts venue.location
+# #   puts
+# # end
 
-venue = nyc_venues[10..-1].shuffle.first
+# venue = nyc_venues[10..-1].shuffle.first #Randomly produce venue in top 50 but not in the top 10
+# venue = arts.shuffle.first
+venue = night.shuffle.first
 puts venue.fid
 puts venue.name
 puts venue.location
+puts venue.rating
+puts venue.url
+puts venue.photourl
+puts venue.price
 puts
